6d934f5d52318a32851d5b36cab4aa2b9b784f97
Ural <ural-islamov@mail.ru>
2022-03-14 21:21:05 +0500

    add stars for grade

--- src/components/pages/p6-cards/Card.tsx
+++ src/components/pages/p6-cards/Card.tsx
@@ -4,6 +4,8 @@ import {CardType, deleteCard} from "../../../bll/reducers/r6-cards/cards-reducer
 import {useDispatch, useSelector} from "react-redux";
 import {AppStateType} from "../../../bll/store";
 import {EditCardModal} from "./EditCardModal/EditCardModal";
+import fullStar from '../../../common/images/FullStar.svg'
+import emptyStar from '../../../common/images/EmptyStar.svg'
 
 type PropsType = {
     card: CardType
@@ -29,11 +31,13 @@ export const Card = ({card, ...props}: PropsType) => {
             <div className={styles.question}>{card.question}</div>
             <div className={styles.answer}>{card.answer}</div>
             <div className={styles.updated}>{card.updated.slice(0, 10).split('-').reverse().join('.')}</div>
-            <div className={styles.grade}>{card.grade}</div>
+            <div className={styles.grade}><Grades grade={card.grade}/></div>
             {userId === card.user_id &&
             <div className={styles.buttons}>
                 <div>
-                    <button className={styles.dellBtn} onClick={() => deleteCardBtn(card.cardsPack_id, card._id)}>Delete</button>
+                    <button className={styles.dellBtn}
+                            onClick={() => deleteCardBtn(card.cardsPack_id, card._id)}>Delete
+                    </button>
                 </div>
                 <div>
                     <button onClick={openModal}>Edit</button>
@@ -42,6 +46,20 @@ export const Card = ({card, ...props}: PropsType) => {
             }
             {wantToEdit && <EditCardModal closeModal={closeModal} packId={card.cardsPack_id}
                                           cardId={card._id} question={card.question} answer={card.answer}/>}
+        </div>
+    )
+}
+
+const Grades = (props: any) => {
+    return (
+        <div>
+            <div className={styles.star}>
+                <img src={props.grade > 1 ? fullStar : emptyStar} alt=""/>
+                <img src={props.grade > 2 ? fullStar : emptyStar} alt=""/>
+                <img src={props.grade > 3 ? fullStar : emptyStar} alt=""/>
+                <img src={props.grade > 4 ? fullStar : emptyStar} alt=""/>
+                <img src={props.grade > 5 ? fullStar : emptyStar} alt=""/>
+            </div>
         </div>
     )
 }
